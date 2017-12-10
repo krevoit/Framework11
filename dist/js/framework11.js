9ebbc594bc31bc52740f3c2e551daca4cabb1548
1GamerDev <loftynine7+1gdgithub@hotmail.com>
2017-12-10 13:22:07 +0000

    Update framework11.js

--- dist/js/framework11.js
+++ dist/js/framework11.js
@@ -19,3 +19,12 @@ function loadPage(func, url) {
     throw "Invalid Function"
   }
 }
+function linkToPageLink() {
+for(var acount = 0; document.getElementsByTagName("a").length >= acount; acount++) {
+try {
+ if(!document.getElementsByTagName("a")[acount].classList.contains("external") && document.getElementsByTagName("a")[acount].href.search("#") == -1 && document.getElementsByTagName("a")[acount].href.search("javascript:") == -1) {
+   document.getElementsByTagName("a")[acount].href = "javascript:loadPage(0, '" + document.getElementsByTagName("a")[acount].href + "');";
+ }
+} catch(err) { }
+}
+}
