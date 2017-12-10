2418225d8c6d2776b3e3c48c80ee49d223f6213c
Warsame <heyitswarsame@gmail.com>
2017-12-10 09:34:34 -0500

    add js

---
+++ dist/js/framework11.js
@@ -0,0 +1,37 @@
+Function Framework11() {
+    var F11 = this;
+    F11.loader;
+
+
+    F11.loadPage = function(func, url) {
+        if (!func && func !== 0) {
+            throw "Function Required: 0 loads page, 1 dismisses page.";
+        }
+        if (!url && func !== 1) {
+            throw "URL Required";
+        }
+        if (func == 0) {
+            if (url !== F11.loader) {
+                $("#page").load(url);
+                F11.loader = url;
+            }
+            document.getElementById("page").className = "page-open";
+        } else if (func == 1) {
+            document.getElementById("page").className = "page-closed";
+        } else {
+            throw "Invalid Function";
+        }
+    }
+    F11.linkToPageLink = function() {
+        for (var acount = 0; document.getElementsByTagName("a").length >= acount; acount++) {
+            try {
+                if (!document.getElementsByTagName("a")[acount].classList.contains("external") && document.getElementsByTagName("a")[acount].href.search("#") == -1 && document.getElementsByTagName("a")[acount].href.search("javascript:") == -1) {
+                    document.getElementsByTagName("a")[acount].href = "javascript:loadPage(0,'" + document.getElementsByTagName("a")[acount].href + "');";
+                }
+            } catch (err) {}
+        }
+    }
+    linkToPageLink();
+
+
+}
